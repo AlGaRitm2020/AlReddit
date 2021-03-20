491637d1092df74792b19ce36c92d03d09d5aaaf
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-03-20 13:57:20 +0400

    make_response function

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+from tools.misc import make_resp, check_keys
+from users.repo import InMemoryUsersRepo
 
 app = Flask(__name__)
+app.user_repo = InMemoryUsersRepo()
 
 
 @app.route("/")
 def main():
     return app.send_static_file('index.html')
 
+
 @app.route("/api/register", methods=["POST"])
 def user_register():
-    pass
+    in_json = request.json
+    if not in_json:
+        return make_resp(jsonify({'message': "Empty request"}), 400)
+    elif not check_keys(in_json, ('username', "password")):
+        return make_resp((jsonify({'message': "Bad request"})), 400)
+    created_user = app.user_repo.request_create(**in_json)
+    if created_user is None:
+        return make_resp(jsonify({'message': 'Dublication user'}), 400)
 
 
 if __name__ == '__main__':
