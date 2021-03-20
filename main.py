e55c34774573fb582616ac6b2d58942161105c5c
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-03-20 14:16:19 +0400

    registration( not working)

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, request, jsonify
+from datetime import timedelta
 
-from tools.misc import make_resp, check_keys
+from flask import Flask, request, jsonify
+from flask_jwt_simple import JWTManager
+
+from tools.misc import make_resp, check_keys, create_jwt_generate_response
 from users.repo import InMemoryUsersRepo
 
 app = Flask(__name__)
 app.user_repo = InMemoryUsersRepo()
+app.config["JWT_SECRET_KEY"] = 'super-secret-str'
+app.config["JWT_EXPIRES"] = timedelta(hours=24)
+app.config["JWT_IDENTITY_CLAIM"] = 'user'
+app.config["JWT_HEADER_NAME"] = 'authorization'
+app.jwt = JWTManager(app)
 
 
 @app.route("/")
@@ -21,7 +29,8 @@ def user_register():
         return make_resp((jsonify({'message': "Bad request"})), 400)
     created_user = app.user_repo.request_create(**in_json)
     if created_user is None:
-        return make_resp(jsonify({'message': 'Dublication user'}), 400)
+        return make_resp(jsonify({'message': 'Duplication user'}), 400)
+    return create_jwt_generate_response(created_user)
 
 
 if __name__ == '__main__':
--- tools/misc.py
+++ tools/misc.py
@@ -1,10 +1,21 @@
-from flask import make_response
+from flask import make_response, jsonify
+from flask_jwt_simple import create_jwt
+
+from users.user import User
 
 
 def make_resp(message, status):
     resp = make_response(message, status)
-    resp.headers['Content-type'] = "applicaton/json; charset=utf-8"
+    resp.headers['Content-type'] = "application/json; charset=utf-8"
     return resp
+
 
 def check_keys(dct, keys):
     return all(key in dct for key in keys)
+
+
+def create_jwt_generate_response(user):
+    cp_user = User(**user)
+    del cp_user['password']
+    j_token = {'token': create_jwt(identity=cp_user)}
+    return make_resp(jsonify(j_token), 200)
